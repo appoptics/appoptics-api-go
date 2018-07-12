629a358966578ed5ee3c514d684ea4c8d0eeba86
joemadeus <jrose@solarwinds.cloud>
2018-07-12 08:52:11 -0400

    Status code as tag

--- contrib/grpc/interceptors.go
+++ contrib/grpc/interceptors.go
@@ -12,71 +12,63 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type instrumentedServer struct {
+type InstrumentedServer struct {
 	m       *appoptics.MeasurementSet
 	service string
 	method  string
 	tags    map[string]interface{}
 }
 
-func newInstrumentedServer(m *appoptics.MeasurementSet, service, method string, tags map[string]interface{}) *instrumentedServer {
-	return &instrumentedServer{
-		m:       m,
-		service: service,
-		method:  method,
-		tags:    tags,
-	}
-}
-
-func (s *instrumentedServer) key(key string) string {
+func (s *InstrumentedServer) key(key string) string {
 	return appoptics.MetricWithTags(fmt.Sprintf("%s.%s.%s", s.service, s.method, key), s.tags)
 }
 
-func (s *instrumentedServer) sent() {
+func (s *InstrumentedServer) sent() {
 	s.m.Incr(s.key("sent"))
 }
 
-func (s *instrumentedServer) received() {
+func (s *InstrumentedServer) received() {
 	s.m.Incr(s.key("received"))
 }
 
-func (s *instrumentedServer) handled(err error) {
-	s.m.Incr(s.key(status.Code(err).String()))
+func (s *InstrumentedServer) handled(err error) {
+	s.tags["status"] = status.Code(err).String()
+	s.m.Incr(s.key("result"))
 }
 
-func (s *instrumentedServer) timed(t time.Duration) {
+func (s *InstrumentedServer) timed(t time.Duration) {
 	s.m.UpdateAggregatorValue(s.key("time_ms"), float64(t/time.Millisecond)+float64(t%time.Millisecond)/1e9)
 }
 
-type instrumentedServerStream struct {
+type InstrumentedServerStream struct {
 	grpc.ServerStream
-	*instrumentedServer
+	InstrumentedServer
 }
 
-func (s *instrumentedServerStream) SendMsg(m interface{}) error {
+func (s *InstrumentedServerStream) SendMsg(m interface{}) error {
 	err := s.ServerStream.SendMsg(m)
 	if err == nil {
-		s.instrumentedServer.sent()
+		s.InstrumentedServer.sent()
 	}
 	return err
 }
 
-func (s *instrumentedServerStream) RecvMsg(m interface{}) error {
+func (s *InstrumentedServerStream) RecvMsg(m interface{}) error {
 	err := s.ServerStream.RecvMsg(m)
 	if err == nil {
-		s.instrumentedServer.received()
+		s.InstrumentedServer.received()
 	}
 	return err
 }
 
 func UnaryServerInterceptor(m *appoptics.MeasurementSet) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		instrument := newInstrumentedServer(
+		instrument := InstrumentedServer{
 			m,
 			path.Dir(info.FullMethod)[1:],
 			path.Base(info.FullMethod),
 			ctxtags.Extract(ctx).Values(),
-		)
+		}
 
 		instrument.received()
 
@@ -95,14 +87,14 @@ func UnaryServerInterceptor(m *appoptics.MeasurementSet) grpc.UnaryServerInterce
 
 func StreamServerInterceptor(m *appoptics.MeasurementSet) grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		instrument := &instrumentedServerStream{
+		instrument := &InstrumentedServerStream{
 			ss,
-			newInstrumentedServer(
+			InstrumentedServer{
 				m,
 				path.Dir(info.FullMethod)[1:],
 				path.Base(info.FullMethod),
 				nil,
-			),
+			},
 		}
 
 		start := time.Now()
