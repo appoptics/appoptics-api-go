8d7446bb93fcbc0e9f2e821ffc8fd4aa9ef18e49
joemadeus <jrose@solarwinds.cloud>
2018-07-12 11:47:53 -0400

    Adding a test for the unary interceptor

---
+++ contrib/grpc/interceptors_test.go
@@ -0,0 +1,35 @@
+package interceptors
+
+import (
+	"context"
+	"testing"
+
+	"google.golang.org/grpc"
+	"github.com/appoptics/appoptics-api-go"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+	"strings"
+)
+
+var (
+	srv = "server_full_of_testing"
+	uInfo = &grpc.UnaryServerInfo{
+		FullMethod: "/something/blah",
+		Server:     srv,
+	}
+)
+
+func TestUnaryRequest(t *testing.T) {
+	measures := appoptics.NewMeasurementSet()
+	intercept := UnaryServerInterceptor(measures)
+	ctx := context.Background()
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return "something", nil
+	}
+	intercept(ctx, "some data", uInfo, handler)
+
+	stupidTestShenanigans := fmt.Sprintf("%v", measures)
+	assert.NotNil(t, strings.Contains(stupidTestShenanigans, "something.blah.received"))
+	assert.NotNil(t, strings.Contains(stupidTestShenanigans, "something.blah.result::status::OK"))
+	assert.NotNil(t, strings.Contains(stupidTestShenanigans, "something.blah.time_ms"))
+}
